860ba99e574831c38a553909d2d510ffca21b830
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2021-12-19 11:05:31 -0600

    Agregar archivos de proyecto.

---
+++ DatabindingTutorial.WinformUI/FrmPrincipal.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace DatabindingTutorial.WinformUI
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.airplaneBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fuelLeftKgDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.airplaneBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.AutoGenerateColumns = false;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.fuelLeftKgDataGridViewTextBoxColumn,
+            this.modelDataGridViewTextBoxColumn});
+            this.grid.DataSource = this.airplaneBindingSource;
+            this.grid.Location = new System.Drawing.Point(12, 79);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(724, 258);
+            this.grid.TabIndex = 0;
+            // 
+            // txtModel
+            // 
+            this.txtModel.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.airplaneBindingSource, "Model", true));
+            this.txtModel.Location = new System.Drawing.Point(110, 27);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(382, 22);
+            this.txtModel.TabIndex = 1;
+            // 
+            // airplaneBindingSource
+            // 
+            this.airplaneBindingSource.DataSource = typeof(DatabindingTutorial.WinformUI.Airplane);
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // fuelLeftKgDataGridViewTextBoxColumn
+            // 
+            this.fuelLeftKgDataGridViewTextBoxColumn.DataPropertyName = "FuelLeftKg";
+            this.fuelLeftKgDataGridViewTextBoxColumn.HeaderText = "FuelLeftKg";
+            this.fuelLeftKgDataGridViewTextBoxColumn.Name = "fuelLeftKgDataGridViewTextBoxColumn";
+            // 
+            // modelDataGridViewTextBoxColumn
+            // 
+            this.modelDataGridViewTextBoxColumn.DataPropertyName = "Model";
+            this.modelDataGridViewTextBoxColumn.HeaderText = "Model";
+            this.modelDataGridViewTextBoxColumn.Name = "modelDataGridViewTextBoxColumn";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 391);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.grid);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.airplaneBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fuelLeftKgDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modelDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource airplaneBindingSource;
+    }
+}
+
---
+++ DatabindingTutorial.WinformUI/Passenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabindingTutorial.WinformUI
+{
+    class Passenger
+    {
+        public Passenger(string name)
+        {
+            _id = ++lastID; Name = name;
+        }
+        private static int lastID = 0;
+        public int _id;
+        public int ID { get { return _id; } }
+        public string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+    }
+}
